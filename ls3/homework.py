475bc729f6a4562fe3d8b6f24965713b47747b9a
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2022-10-01 11:50:39 +0300

    ls3

---
+++ ls3/homework.py
@@ -0,0 +1,7 @@
+city_1 = input("Enter city: ")
+city_2 = input("Enter city: ")
+city_3 = input("Enter city: ")
+city_4 = input("Enter city: ")
+city_5 = input("Enter city: ")
+print(city_1, city_2, city_3, city_4, city_5)
+
