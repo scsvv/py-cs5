160ea242b8cd61a92c4e991f28520fcf49d5f639
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2022-10-24 18:59:39 +0300

    ls10

---
+++ ls10/main.py
@@ -0,0 +1,30 @@
+"""
+Пользователь вводит число K - количество фруктов. Затем он вводит K фруктов в формате: название фрукта и его количество. Добавьте все фрукты в словарь, где название фрукта - это ключ, а количество - значение. Например: 
+"""
+# None, null, undefined
+
+value = None
+if (value == None) {
+
+}
+
+while 1: 
+    K = input("Enter number: ")
+    
+
+    try: 
+        K = int(K)
+        break
+    except ValueError: 
+        print("Введите число!!!")
+    
+
+goods_quantity = {}
+
+for i in range(K):
+    name = input("Fruit's name: ")
+    count= int(input("Quantity: "))
+
+    goods_quantity[name] = count
+
+print(goods_quantity)
